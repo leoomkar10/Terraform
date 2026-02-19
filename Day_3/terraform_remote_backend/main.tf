42e54ea6f60b789e3cf952e3fa931ce1cd672fd8
omkar gunjote <srising668@gmail.com>
2026-02-19 20:43:02 +0530

    Updated Subnet ID

--- Day_3/terraform_remote_backend/main.tf
+++ Day_3/terraform_remote_backend/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_ec2_instance" {
     ami = "ami-051a31ab2f4d498f5"
     instance_type = "t2.micro"
-    subnet_id = "subnet-05736e334de12dca1"
+    subnet_id = "****************"
     tags = {
         Name = "Terraform EC2"
     }
